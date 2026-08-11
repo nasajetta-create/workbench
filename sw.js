01615e4403ae72c1cd9ee75d74ef0ba4a2bb7f4b
nasajetta <nasa.jetta@gmail.com>
2026-08-11 17:45:01 +0800

    W0811-11 貸款利率/到期日+手動還款型（sw wb-v20）

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 艋舺良的工作台 service worker（強制自動更新版，沿用旅遊分帳做法） */
-const CACHE = 'wb-v19';
+const CACHE = 'wb-v20';
 const CORE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
